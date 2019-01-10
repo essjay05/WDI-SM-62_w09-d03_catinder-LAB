97f78579c72761ec6b68eb233bc617fa96c57924
Joy <stepjoyous@gmail.com>
2019-01-09 16:29:27 -0800

    Created Cat model and Cats controller

--- catinder/config/routes.rb
+++ catinder/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'cats/index'
+  get 'cats/show'
+  get 'cats/new'
+  get 'cats/create'
+  get 'cats/edit'
+  get 'cats/update'
+  get 'cats/delete'
   # Root/home page
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
